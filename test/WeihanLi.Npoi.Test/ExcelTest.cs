abe2d174d1462d6cd79f146ccea7316fe9dcf180
WeihanLi <weihanli@outlook.com>
2019-10-23 15:05:55 +0800

    add BasicImportExportWithoutHeaderTest

--- test/WeihanLi.Npoi.Test/ExcelTest.cs
+++ test/WeihanLi.Npoi.Test/ExcelTest.cs
@@ -49,6 +49,52 @@ namespace WeihanLi.Npoi.Test
         }
 
         [Fact]
+        public void BasicImportExportWithoutHeaderTest()
+        {
+            var noticeSetting = ExcelHelper.SettingFor<Notice>();
+            lock (noticeSetting)
+            {
+                noticeSetting.HasSheetConfiguration(0, "test", 0);
+                var list = new List<Notice>();
+                for (var i = 0; i < 10; i++)
+                {
+                    list.Add(new Notice()
+                    {
+                        Id = i + 1,
+                        Content = $"content_{i}",
+                        Title = $"title_{i}",
+                        PublishedAt = DateTime.UtcNow.AddDays(-i),
+                        Publisher = $"publisher_{i}"
+                    });
+                }
+                list.Add(new Notice() { Title = "nnnn" });
+                list.Add(null);
+
+                var excelBytes = list.ToExcelBytes();
+
+                var importedList = ExcelHelper.ToEntityList<Notice>(excelBytes);
+                Assert.Equal(list.Count, importedList.Count);
+                for (var i = 0; i < list.Count; i++)
+                {
+                    if (list[i] == null)
+                    {
+                        Assert.Null(importedList[i]);
+                    }
+                    else
+                    {
+                        Assert.Equal(list[i].Id, importedList[i].Id);
+                        Assert.Equal(list[i].Title, importedList[i].Title);
+                        Assert.Equal(list[i].Content, importedList[i].Content);
+                        Assert.Equal(list[i].Publisher, importedList[i].Publisher);
+                        Assert.Equal(list[i].PublishedAt.ToStandardTimeString(), importedList[i].PublishedAt.ToStandardTimeString());
+                    }
+                }
+
+                noticeSetting.HasSheetConfiguration(0, "test", 1);
+            }
+        }
+
+        [Fact]
         public void ImportWithNotSpecificColumnIndex()
         {
             IReadOnlyList<Notice> list = Enumerable.Range(0, 10).Select(i => new Notice()
