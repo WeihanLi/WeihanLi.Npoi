1034d804e040a48a910d7d9c4585bbe63619865c
WeihanLi <weihanli@outlook.com>
2019-10-23 14:56:51 +0800

    add ImportWithNotSpecificColumnIndex test clean code

--- test/WeihanLi.Npoi.Test/ExcelTest.cs
+++ test/WeihanLi.Npoi.Test/ExcelTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using WeihanLi.Extensions;
 using WeihanLi.Npoi.Test.Models;
 using Xunit;
@@ -45,6 +46,49 @@ namespace WeihanLi.Npoi.Test
                     Assert.Equal(list[i].PublishedAt.ToStandardTimeString(), importedList[i].PublishedAt.ToStandardTimeString());
                 }
             }
+        }
+
+        [Fact]
+        public void ImportWithNotSpecificColumnIndex()
+        {
+            IReadOnlyList<Notice> list = Enumerable.Range(0, 10).Select(i => new Notice()
+            {
+                Id = i + 1,
+                Content = $"content_{i}",
+                Title = $"title_{i}",
+                PublishedAt = DateTime.UtcNow.AddDays(-i),
+                Publisher = $"publisher_{i}"
+            }).ToArray();
+            var excelBytes = list.ToExcelBytes();
+            //
+            var noticeSetting = ExcelHelper.SettingFor<Notice>();
+            noticeSetting.Property(_ => _.Publisher)
+                .HasColumnIndex(4);
+            noticeSetting.Property(_ => _.PublishedAt)
+                .HasColumnIndex(3);
+
+            var importedList = ExcelHelper.ToEntityList<Notice>(excelBytes);
+            Assert.Equal(list.Count, importedList.Count);
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i] == null)
+                {
+                    Assert.Null(importedList[i]);
+                }
+                else
+                {
+                    Assert.Equal(list[i].Id, importedList[i].Id);
+                    Assert.Equal(list[i].Title, importedList[i].Title);
+                    Assert.Equal(list[i].Content, importedList[i].Content);
+                    Assert.Equal(list[i].Publisher, importedList[i].Publisher);
+                    Assert.Equal(list[i].PublishedAt.ToStandardTimeString(), importedList[i].PublishedAt.ToStandardTimeString());
+                }
+            }
+
+            noticeSetting.Property(_ => _.Publisher)
+                .HasColumnIndex(3);
+            noticeSetting.Property(_ => _.PublishedAt)
+                .HasColumnIndex(4);
         }
     }
 }
