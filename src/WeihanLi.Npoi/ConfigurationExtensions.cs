dd1b3bb1c03b222ba526d8934a6eb3002c87bf84
WeihanLi <weihanli@outlook.com>
2020-01-14 14:24:04 +0800

    clean code, remove ExcelSettings from IExcelConfiguration update missing document

--- src/WeihanLi.Npoi/ConfigurationExtensions.cs
+++ src/WeihanLi.Npoi/ConfigurationExtensions.cs
@@ -5,20 +5,40 @@ namespace WeihanLi.Npoi
 {
     public static class ConfigurationExtensions
     {
+        /// <summary>
+        /// Sheet Configuration
+        /// </summary>
+        /// <param name="configuration">excel configuration</param>
+        /// <param name="sheetIndex">sheetIndex</param>
+        /// <param name="sheetName">sheetName</param>
+        /// <returns>current excel configuration<see cref="IExcelConfiguration"/></returns>
         public static IExcelConfiguration HasSheetConfiguration(this IExcelConfiguration configuration, int sheetIndex,
-            string sheetName)
-            => configuration.HasSheetConfiguration(sheetIndex, sheetName, 1, false);
-
-        public static IExcelConfiguration HasSheetConfiguration(this IExcelConfiguration configuration, int sheetIndex,
-            string sheetName, bool enableAutoColumnWidth)
-            => configuration.HasSheetConfiguration(sheetIndex, sheetName, 1, enableAutoColumnWidth);
-
-        public static IExcelConfiguration HasSheetConfiguration(this IExcelConfiguration configuration, int sheetIndex,
-            string sheetName, int startRowIndex) =>
-            configuration.HasSheetConfiguration(sheetIndex, sheetName, startRowIndex, false);
+            string sheetName) => configuration.HasSheetConfiguration(sheetIndex, sheetName, 1, false);
 
         /// <summary>
-        /// get excelConfiguration property configuration
+        /// Sheet Configuration
+        /// </summary>
+        /// <param name="configuration">excel configuration</param>
+        /// <param name="sheetIndex">sheetIndex</param>
+        /// <param name="sheetName">sheetName</param>
+        /// <param name="enableAutoColumnWidth">enable auto column width if true otherwise false</param>
+        /// <returns>current excel configuration<see cref="IExcelConfiguration"/></returns>
+        public static IExcelConfiguration HasSheetConfiguration(this IExcelConfiguration configuration, int sheetIndex,
+            string sheetName, bool enableAutoColumnWidth) => configuration.HasSheetConfiguration(sheetIndex, sheetName, 1, enableAutoColumnWidth);
+
+        /// <summary>
+        /// Sheet Configuration
+        /// </summary>
+        /// <param name="configuration">excel configuration</param>
+        /// <param name="sheetIndex">sheetIndex</param>
+        /// <param name="sheetName">sheetName</param>
+        /// <param name="startRowIndex">startRowIndex</param>
+        /// <returns>current excel configuration<see cref="IExcelConfiguration"/></returns>
+        public static IExcelConfiguration HasSheetConfiguration(this IExcelConfiguration configuration, int sheetIndex,
+            string sheetName, int startRowIndex) => configuration.HasSheetConfiguration(sheetIndex, sheetName, startRowIndex, false);
+
+        /// <summary>
+        /// property configuration
         /// </summary>
         /// <typeparam name="TEntity">TEntity</typeparam>
         /// <param name="excelConfiguration">excelConfiguration</param>
