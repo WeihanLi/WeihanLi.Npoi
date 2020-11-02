52333c6b252d3b820e5138426c1edb2fa413022e
weihanli <weihanli@outlook.com>
2020-11-03 01:00:34 +0800

    update sample

--- samples/DotNetCoreSample/Program.cs
+++ samples/DotNetCoreSample/Program.cs
@@ -16,6 +16,11 @@ namespace DotNetCoreSample
             FluentSettingsForExcel();
             var tempDirPath = $@"{Environment.GetEnvironmentVariable("USERPROFILE")}\Desktop\temp\test";
 
+            //FluentSettings.For<ppDto>()
+            //    .HasSheetSetting(sheet =>
+            //    {
+            //        sheet.CellFilter = cell => cell.ColumnIndex <= 10;
+            //    });
             var tempExcelPath = Path.Combine(tempDirPath, "testdata.xlsx");
             var t_list = ExcelHelper.ToEntityList<ppDto>(tempExcelPath);
             //var tempTable = ExcelHelper.ToDataTable(tempExcelPath);
