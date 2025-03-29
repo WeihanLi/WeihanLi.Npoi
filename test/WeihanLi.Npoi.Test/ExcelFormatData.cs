5817f0a5743a6e84e0ff3c158658f772dae2b0e2
Weihan Li <weihanli@outlook.com>
2025-03-29 18:53:46 +0800

    feat: xunit v3

---
+++ test/WeihanLi.Npoi.Test/ExcelFormatData.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the Apache license.
+
+using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+using Xunit.v3;
+
+namespace WeihanLi.Npoi.Test;
+
+public sealed class ExcelFormatData : TheoryData<ExcelFormat>
+{
+    public ExcelFormatData()
+    {
+        Add(ExcelFormat.Xls);
+        Add(ExcelFormat.Xlsx);
+    }
+}
