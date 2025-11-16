4c44b6a1d82964656ecb8a2fb9146e5a1869b98b
Weihan Li <weihanli@outlook.com>
2025-11-16 21:03:43 +0800

    build: specify the package version in build script

--- build.cs
+++ build.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Weihan Li. All rights reserved.
 // Licensed under the Apache license.
 
-#:package WeihanLi.Common
+#:package WeihanLi.Common@1.0.84
 
 using WeihanLi.Common.Helpers;
 
